b0b7490e308d45e211eba8d725170eb531cebff7
Valeria Mora <valemora2018@gmail.com>
2023-11-15 23:58:27 -0600

    Condicionales 4 ejercicios

---
+++ index.js
@@ -0,0 +1,66 @@
+//EJERCICIO 1
+
+const respuesta = prompt("¿Eres bellisimo/a?").toLowerCase();
+
+if (respuesta === "si") {
+    console.log ("Ciertamente"); 
+}
+else if (respuesta === "no") {
+    console.log ("No te creo");
+}
+else {
+    console.log ("Respuesta no válida");
+}
+
+//EJERCICIO 2 ---------------------------------------------------------------->
+
+const numero = prompt("Ingrese un número, porfis");
+
+if (numero % 2 === 0) {
+    console.log (`${numero} es divisible entre 2`);
+}
+else {
+    console.log (`${numero} no es divisible entre 2`)
+}
+
+//EJERCICIO 3 ----------------------------------------------------------------->
+
+const num = prompt("Ingresa otro número");
+
+if (num % 2 === 0) {
+    alert ("ES PAR")
+}
+else {
+    alert ("ES IMPAR")
+}
+
+//EJERCICIO 4 ----------------------------------------------------------------->
+
+const numGanador = prompt("Ingresa un número para ver si ganas un premio")
+
+if (numGanador === 1000) {
+    console.log ("Ganaste un premio!");
+}
+else {
+    console.log ("Lo sentimos, sigue participando");
+}
+
+//EJERCICIO 5 ----------------------------------------------------------------->
+
+
+//EJERCICIO 6 ----------------------------------------------------------------->
+
+
+//EJERCICIO 7 ----------------------------------------------------------------->
+
+
+//EJERCICIP 8 ----------------------------------------------------------------->
+
+
+//EJERCICIO 9 ----------------------------------------------------------------->
+
+
+//EJERCICIO 10 ----------------------------------------------------------------->
+
+
+//EJERCICIO 11 ----------------------------------------------------------------->
